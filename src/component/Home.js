79c8c3031db620db36e3b562ab082e56159c596c
Gaurav Shreenet <gthakuri656@gmail.com>
2023-10-17 23:51:31 +1100

    added the image

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import imagePortfolio from "../asset/My project.png"
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className="container" id='home'>
+        <div className="row mt-5 p-5">
+            <div className="col">
+                sadasdasdas
+            </div>
+            <div className="col bg-danger rounded-circle">
+                <div className="image-resize d-flex justify-content-center mb-5">
+                    <img src={imagePortfolio} alt="" />
+                </div>
+            </div>
+        </div>
+        
+    </div>
   )
 }
