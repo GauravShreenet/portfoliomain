b496d9371877791516107d2102de01ca0db7c009
Gaurav Shreenet <gthakuri656@gmail.com>
2023-10-25 22:12:55 +1100

    added animated vertical line

--- src/component/Experience.js
+++ src/component/Experience.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import { motion ,useScroll } from 'framer-motion'
+import React, { useRef } from 'react'
 
 const Details = ({ postion, company, companyLink, time, address, work }) => {
     return <li className='mb-5 d-flex flex-column align-items-center justify-content-between'>
@@ -18,6 +19,13 @@ const Details = ({ postion, company, companyLink, time, address, work }) => {
 }
 
 export const Experience = () => {
+    const ref = useRef(null);
+    const {scrollYProgress} = useScroll(
+        {
+            target: ref,
+            offset: ["start end", "center start"]
+        }
+    )
     return (
         <div className="container">
             <div className="row">
@@ -25,12 +33,39 @@ export const Experience = () => {
                     <h1 className='fw-bolder'>Experience</h1>
                 </div>
             </div>
-            <div className='row mt-5'>
+            <div ref={ref} className='row mt-5'>
                 <div className='col-1 d-flex justify-content-center'>
-                    <div className='postion-absolute top-0 bottom-0' style={{width: '4px', background: 'var(--text-color)'}}></div>
+                    <motion.div 
+                    className='postion-absolute top-0 bottom-0' style={{width: '4px', background: 'var(--text-color)', scaleY: scrollYProgress, transformOrigin: 'top'}} />
                 </div>
                 <div className="col-11">
                 <ul className='d-flex flex-column align-items-start justify-content-between'>
+                    <Details
+                        postion="Support Technician" company="Reliable IT Solutions"
+                        companyLink="https://www.reliableitsolutions.com.au/"
+                        time="May 2022 - Aug 2022" address="2A Boyle Street, Sutherland, NSW 2232"
+                        work="Operating System troubleshooting. Building workstations according to the customer's needs. Software and Hardware problem-solving."
+                    />
+
+                    <Details
+                        postion="Grocery Supervisor" company="The Greener Grocer"
+                        companyLink=""
+                        time="Apr 2023 - Present" address="Maroubra, New South Wales, Australia"
+                        work="oversees daily operations in a grocery store, managing staff, inventory, and customer service. Responsible for maintaining stock levels, ensuring a positive shopping experience, and meeting sales and budget goals while adhering to safety and regulatory standards."
+                    />
+                    <Details
+                        postion="Support Technician" company="Reliable IT Solutions"
+                        companyLink="https://www.reliableitsolutions.com.au/"
+                        time="May 2022 - Aug 2022" address="2A Boyle Street, Sutherland, NSW 2232"
+                        work="Operating System troubleshooting. Building workstations according to the customer's needs. Software and Hardware problem-solving."
+                    />
+
+                    <Details
+                        postion="Grocery Supervisor" company="The Greener Grocer"
+                        companyLink=""
+                        time="Apr 2023 - Present" address="Maroubra, New South Wales, Australia"
+                        work="oversees daily operations in a grocery store, managing staff, inventory, and customer service. Responsible for maintaining stock levels, ensuring a positive shopping experience, and meeting sales and budget goals while adhering to safety and regulatory standards."
+                    />
                     <Details
                         postion="Support Technician" company="Reliable IT Solutions"
                         companyLink="https://www.reliableitsolutions.com.au/"
