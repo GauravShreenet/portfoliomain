bbcb12c0c4d967aa6ae4f66cba17bc9a522d5efe
Gaurav Shreenet <gthakuri656@gmail.com>
2023-10-26 17:14:39 +1100

    animated the skills coming out

--- src/component/LiIcon.js
+++ src/component/LiIcon.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { motion, useScroll } from 'framer-motion'
 
-export const LiIcon = (reference) => {
+export const LiIcon = ({reference}) => {
   const { scrollYProgress } = useScroll(
     {
       target: reference,
--- src/component/Skills.js
+++ src/component/Skills.js
@@ -7,8 +7,9 @@ const Skill = ({ name, x, y }) => {
             '
             whileHover={{ scale: 1.05 }}
             initial={{ x: 0, y: 0 }}
-            animate={{ x: x, y: y }}
+            whileInView={{ x: x, y: y }}
             transition={{ duration: 1.5 }}
+            viewport={{once: true}}
             style={{ fontSize: '7rem' }}
         >
             {name}
@@ -21,17 +22,17 @@ export const Skills = () => {
         <>
             <h1 className='fw-bold mt-5 text-center'>Skills</h1>
             <div className='position-relative d-flex align-items-center justify-content-center vh-100'>
-                <motion.div className='d-flex align-items-center justify-content-center bg-success'
+                <motion.div className='d-flex align-items-center justify-content-center'
                     style={{ cursor: 'pointer', fontSize: '7rem' }}
                 >
                     <i className="fa-solid fa-globe" whileHover={{ scale: 1.05 }}></i>
                 </motion.div>
 
-                <Skill name={<i class="fa-brands fa-html5"></i>} x="-20vw" y="2vw" />
-                <Skill name={<i className='fa-brands fa-css3-alt'></i>} x="-5vw" y="-10vw" />
-                <Skill name={<i class="fa-brands fa-square-js"></i>} x="20vw" y="6vw" />
-                <Skill name={<i class="fa-brands fa-react"></i>} x="0vw" y="12vw" />
-                <Skill name={<i class="fa-brands fa-github"></i>} x="-20vw" y="-15vw" />
+                <Skill name={<i class="fa-brands fa-html5" style={{color: '#ff0000', cursor: 'pointer'}}></i>} x="-20vw" y="2vw" />
+                <Skill name={<i className='fa-brands fa-css3-alt' style={{color: '#006eff', cursor: 'pointer'}}></i>} x="-5vw" y="-10vw" />
+                <Skill name={<i class="fa-brands fa-square-js" style={{color: '#fff700', cursor: 'pointer'}}></i>} x="20vw" y="6vw" />
+                <Skill name={<i class="fa-brands fa-react" style={{color: '#00fbef', cursor: 'pointer'}}></i>} x="0vw" y="12vw" />
+                <Skill name={<i class="fa-brands fa-github" style={{color: 'var(--text-color)', cursor: 'pointer'}}></i>} x="10vw" y="-15vw" />
             </div>
         </>
     )
