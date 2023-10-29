dd939bb3cd8eced366c20041041ea0adebaaf5f4
Gaurav Shreenet <gthakuri656@gmail.com>
2023-10-29 21:13:06 +1100

    made some changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,11 +12,11 @@ export const Home = () => {
 
     const opacity = useTransform(scrollYProgress, [0, 0.5], [1, 0]);
     // const opacity1 = useTransform(scrollYProgress, [1, 0.5], [0, 1]);
-    const scale = useTransform(scrollYProgress, [0, 0.5], [1, 0.8]);
+    const scale = useTransform(scrollYProgress, [0, 2], [1, 0.2]);
 
     return (
         <div className="container-md position-relative" id='home'>
-            <motion.div ref={targetRef} className="row position-fixed" style={{ opacity, scale, top: '10%'}}>
+            <motion.div ref={targetRef} className="row position-fixed" style={{ opacity, scale, top: '10%' }}>
                 <div className="col-md d-flex align-items-center">
                     <div className='m-5'>
                         Hi I'm <span className='fs-1 fw-bold'>Gaurav Shreenet</span><br />
@@ -32,51 +32,51 @@ export const Home = () => {
                 </div>
             </motion.div>
 
-            <div style={{ marginTop: '100vh', background: 'green' }}>
-                <div style={{ height: '150vh', width: '100%' }}>
+            <div style={{ marginTop: '100vh'}}>
+                <div style={{ height: '100vh', width: '100%' }}>
                     <div className="overflow-hidden" style={{ position: 'sticky', top: '40vh' }}>
-                        <div className="text-center bg-success" style={{ fontSize: '3rem', fontWeight: 'bold', fontFamily: 'Roboto, sans-serif' }}>
+                        <div className="text-center" style={{ fontSize: '3rem', fontWeight: 'bold', fontFamily: 'Roboto, sans-serif' }}>
                             Transforming ideas into interactive web experiences.
                         </div>
-                        <div>
-                        <div className='contact'>
-                            GET IN
-                        </div>
-                        <div className='contact'>
-                            TOUCH
-                        </div>
                     </div>
-                    </div>
+                    <div style={{marginTop: '28vh', overflow: 'hidden', position: 'sticky', top: '40vh', background: 'var(--body-bg)'}}>
+                            <div className='contact'>
+                                GET IN
+                            </div>
+                            <div className='contact'>
+                                TOUCH
+                            </div>
+                        </div>
                 </div>
             </div>
 
             <div className="row mt-5" id="contact">
-                <div className="col d-flex justify-content-center" style={{ marginTop: '100vh' }}>
-                    
+                <div className="col d-flex justify-content-center" >
+
 
                 </div>
                 <div className="z-2">
-                    <form className='d-flex justify-content-center' style={{marginTop: '25vh'}}>
-                    <div className='w-75'>
-                        <div className="mb-3 mt-5">
-                            <input type="text" className='form-control fs-3 bg-transparent text-white rounded-bottom-0' placeholder='Your Name' id='name' />
+                    <form className='d-flex justify-content-center' style={{ marginTop: '25vh' }}>
+                        <div className='w-75'>
+                            <div className="mb-3 mt-5">
+                                <input type="text" className='form-control fs-3 bg-transparent text-white rounded-bottom-0' placeholder='Your Name' id='name' />
+                            </div>
+                            <div className="mb-3">
+                                <input type="email" className="form-control fs-3 bg-transparent text-white rounded-bottom-0" id="email" placeholder="Your Email" />
+                            </div>
+                            <div className="mb-3">
+                                <input type="text" className='form-control fs-3 bg-transparent text-white rounded-bottom-0' placeholder='Subject' id='subject' />
+                            </div>
+                            <div className="mb-3">
+                                <textarea className="form-control fs-3 bg-transparent text-white rounded-bottom-0" id="textarea" rows="4" placeholder='Your Message' style={{ resize: 'none' }}></textarea>
+                            </div>
+                            <div className='d-flex justify-content-end'>
+                                <button type="submit" className="btn btn-primary fw-bold mt-5">Send Message <i className="fa-solid fa-paper-plane"></i></button>
+                            </div>
                         </div>
-                        <div className="mb-3">
-                            <input type="email" className="form-control fs-3 bg-transparent text-white rounded-bottom-0" id="email" placeholder="Your Email" />
-                        </div>
-                        <div className="mb-3">
-                            <input type="text" className='form-control fs-3 bg-transparent text-white rounded-bottom-0' placeholder='Subject' id='subject' />
-                        </div>
-                        <div className="mb-3">
-                            <textarea className="form-control fs-3 bg-transparent text-white rounded-bottom-0" id="textarea" rows="4" placeholder='Your Message' style={{ resize: 'none' }}></textarea>
-                        </div>
-                        <div className='d-flex justify-content-end'>
-                            <button type="submit" className="btn btn-primary fw-bold mt-5">Send Message <i className="fa-solid fa-paper-plane"></i></button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
                 </div>
-                
+
             </div>
         </div>
     )
--- src/component/NavMenu.js
+++ src/component/NavMenu.js
@@ -1,31 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-export const NavMenu = ({toggleMenu, isOpen}) => {
+export const NavMenu = ({ toggleMenu, isOpen }) => {
   return (
-    <div className='d-flex justify-content-center p-5 m-5'>  
-        <ul className="list-unstyled">
-                            <li>
-                              <div className='overflow-hidden' style={{width: "50vh"}}>
-                              <Link to="/" style={{textDecoration: 'none', color: 'white'}} onClick={()=> toggleMenu(isOpen)}><span className='hover-slide'><i class="fa-solid fa-arrow-right-long me-3" />Home</span></Link>
-                              </div>  
-                            </li>
-                            <li>
-                            <div className='overflow-hidden' style={{width: "50vh"}}>
-                            <Link to="/about" style={{textDecoration: 'none', color: 'white'}} onClick={()=> toggleMenu(isOpen)}><span className='hover-slide'><i class="fa-solid fa-arrow-right-long me-3" />About</span></Link>
-                            </div>
-                            </li>
-                            <li>
-                            <div className='overflow-hidden' style={{width: "50vh"}}>
-                            <Link to="/project" style={{textDecoration: 'none', color: 'white'}} onClick={()=> toggleMenu(isOpen)}><span className='hover-slide'><i class="fa-solid fa-arrow-right-long me-3" />Projects</span></Link>   
-                            </div>
-                            </li>
-                            <li>
-                            <div className='overflow-hidden' style={{width: "50vh"}}>
-                            <Link to="/contact" style={{textDecoration: 'none', color: 'white'}} onClick={()=> toggleMenu(isOpen)}><span className='hover-slide'><i class="fa-solid fa-arrow-right-long me-3" />Contact</span></Link>
-                            </div>
-                            </li>
-                        </ul>
+    <div className='d-flex justify-content-center p-5 m-5'>
+      <ul className="list-unstyled">
+        <li>
+          <div className='overflow-hidden' style={{ width: "50vh" }}>
+            <Link to="/" style={{ textDecoration: 'none', color: 'white' }} onClick={() => toggleMenu(isOpen)}><span className='hover-slide'><i class="fa-solid fa-arrow-right-long me-3" />Home</span></Link>
+          </div>
+        </li>
+        <li>
+          <div className='overflow-hidden' style={{ width: "50vh" }}>
+            <Link to="/about" style={{ textDecoration: 'none', color: 'white' }} onClick={() => toggleMenu(isOpen)}><span className='hover-slide'><i class="fa-solid fa-arrow-right-long me-3" />About</span></Link>
+          </div>
+        </li>
+        <li>
+          <div className='overflow-hidden' style={{ width: "50vh" }}>
+            <Link to="/project" style={{ textDecoration: 'none', color: 'white' }} onClick={() => toggleMenu(isOpen)}><span className='hover-slide'><i class="fa-solid fa-arrow-right-long me-3" />Projects</span></Link>
+          </div>
+        </li>
+        <li>
+          <div className='overflow-hidden' style={{ width: "50vh" }}>
+            <Link to="/contact" style={{ textDecoration: 'none', color: 'white' }} onClick={() => toggleMenu(isOpen)}><span className='hover-slide'><i class="fa-solid fa-arrow-right-long me-3" />Contact</span></Link>
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
