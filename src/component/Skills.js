06b8681f1d78518a122beb2b189fadc147a137bc
Gaurav Shreenet <gthakuri656@gmail.com>
2023-11-08 21:14:47 +1100

    fixed skills section

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -11,7 +11,7 @@ const Skill = ({ name, x, y }) => {
             whileInView={{ x: x, y: y }}
             transition={{ duration: 1.5 }}
             viewport={{once: true}}
-            style={{ fontSize: '7rem' }}
+            style={{ fontSize: '9vh' }}
         >
             {name}
         </motion.div>
@@ -24,16 +24,16 @@ export const Skills = () => {
             <h1 className='fw-bold mt-5 text-center'><RevealAni> Skills</RevealAni></h1>
             <div className='position-relative d-flex align-items-center justify-content-center'>
                 <motion.div className='d-flex align-items-center justify-content-center'
-                    style={{ cursor: 'pointer', fontSize: '7rem', minHeight: '60vw' }}
+                    style={{ cursor: 'pointer', fontSize: '9vh', minHeight: '45vw' }}
                 >
                     <i className="fa-solid fa-globe" whileHover={{ scale: 1.05 }}></i>
                 </motion.div>
 
-                <Skill name={<i class="fa-brands fa-html5" style={{color: '#ff0000', cursor: 'pointer'}}></i>} x="-30vw" y="2vw" />
-                <Skill name={<i className='fa-brands fa-css3-alt' style={{color: '#006eff', cursor: 'pointer'}}></i>} x="-20vw" y="-22vw" />
-                <Skill name={<i class="fa-brands fa-square-js" style={{color: '#fff700', cursor: 'pointer'}}></i>} x="30vw" y="16vw" />
-                <Skill name={<i class="fa-brands fa-react" style={{color: '#00fbef', cursor: 'pointer'}}></i>} x="0vw" y="25vw" />
-                <Skill name={<i class="fa-brands fa-github" style={{color: 'var(--text-color)', cursor: 'pointer'}}></i>} x="20vw" y="-25vw" />
+                <Skill name={<i class="fa-brands fa-html5" style={{color: '#ff0000', cursor: 'pointer'}}></i>} x="-25vw" y="2vw" />
+                <Skill name={<i className='fa-brands fa-css3-alt' style={{color: '#006eff', cursor: 'pointer'}}></i>} x="-15vw" y="-17vw" />
+                <Skill name={<i class="fa-brands fa-square-js" style={{color: '#fff700', cursor: 'pointer'}}></i>} x="25vw" y="10vw" />
+                <Skill name={<i class="fa-brands fa-react" style={{color: '#00fbef', cursor: 'pointer'}}></i>} x="0vw" y="20vw" />
+                <Skill name={<i class="fa-brands fa-github" style={{color: 'var(--text-color)', cursor: 'pointer'}}></i>} x="20vw" y="-20vw" />
             </div>
         </>
     )
